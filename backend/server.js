ccc987ae125b5dca8e40e1bf9110272210d9f22a
Abayomi Aishida <yomicorperate@gmail.com>
2024-08-16 01:38:00 +0100

    Get user and update routes with route protection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
+connectDB();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
