d40b9b09cb200f58acb5ad6bf45e2de90801c6e1
Abayomi Aishida <yomicorperate@gmail.com>
2024-08-16 02:32:35 +0100

    Frontend header and homescreen with react-bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <HomeScreen />
+    </>
+  );
+}
